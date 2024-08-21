95ed4c8d977d7668c1ec6ee3dfe4da1b754321dc
Paviel Barysevich-Bleyaniuk <pavelmessage@gmail.com>
2024-08-21 23:29:25 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -38,8 +38,33 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
-}
+    let arr = expr.split('**********');
+    let decryptedValue = '';
+
+    for (let binaryCode of arr) {
+        let code = binaryCode.split('');
+        let word = '';
+
+        for (let i = 0; i < binaryCode.length / 10; i++) {
+            let tenDigitsCode = code.slice(-10).join('');
+
+            let letter = '';
+
+            for (let j = 0; j < 10; j += 2) {
+                if (tenDigitsCode.slice(j, j + 2) === '10') letter += '.';
+                if (tenDigitsCode.slice(j, j + 2) === '11') letter += '-';
+            }
+
+            code.splice(-10, 10);
+
+            word = `${MORSE_TABLE[letter]}${word}`;
+        }
+
+        decryptedValue += word + ' ';
+    }
+
+    return decryptedValue.trim();
+   }
 
 module.exports = {
     decode
